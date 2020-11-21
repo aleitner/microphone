e41bd065803b8b30311507cbb24eaa0200ad3089
aleitner <leitnersalex@gmail.com>
2020-11-21 16:09:14 -0500

    read from device callback into buffer

--- microphone.go
+++ microphone.go
@@ -1,20 +1,27 @@
 package microphone
 
 import (
+	"sync"
+
 	"github.com/faiface/beep"
 	"github.com/gen2brain/malgo"
 )
 
 func OpenStream(ctx *malgo.AllocatedContext, deviceConfig malgo.DeviceConfig) (*Streamer, beep.Format, error) {
-	var capturedSampleCount uint32
-	pCapturedSamples := make([]byte, 0)
+	s := &Streamer{
+		minBufferSize: 4096,
+	}
+
 	sizeInBytes := uint32(malgo.SampleSizeInBytes(deviceConfig.Capture.Format))
 
-	onRecvFrames := func(pSample2, pSample []byte, framecount uint32) {
+	onRecvFrames := func(outputSample, inputSample []byte, framecount uint32) {
 		sampleCount := framecount * deviceConfig.Capture.Channels * sizeInBytes
-		newCapturedSampleCount := capturedSampleCount + sampleCount
-		pCapturedSamples = append(pCapturedSamples, pSample...)
-		capturedSampleCount = newCapturedSampleCount
+
+		samples := sampleBytesToFloats(inputSample, int(sampleCount))
+		s.buffer = append(s.buffer, samples...)
+		if len(s.buffer) > s.minBufferSize {
+			s.stop()
+		}
 	}
 
 	device, err := malgo.InitDevice(ctx.Context, deviceConfig, malgo.DeviceCallbacks{
@@ -24,27 +31,53 @@ func OpenStream(ctx *malgo.AllocatedContext, deviceConfig malgo.DeviceConfig) (*
 		return nil, beep.Format{}, err
 	}
 
-	s := &Streamer{
-		device: device,
-	}
+	s.device = device
 
 	format := beep.Format{
 		SampleRate:  beep.SampleRate(device.SampleRate()),
 		NumChannels: int(device.CaptureChannels()),
-		Precision:   0,
+		Precision:   3,
 	}
+
+	s.start()
 
 	return s, format, nil
 }
 
 type Streamer struct {
+	mtx    sync.Mutex
 	device *malgo.Device
-	buffer [][]float32
+	buffer [][2]float64
+	minBufferSize int
 	err    error
 }
 
 func (s *Streamer) Stream(samples [][2]float64) (int, bool) {
-	return 0, false
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	// Stream is already empty
+	if len(s.buffer) == 0 {
+		return 0, false
+	}
+
+	numSamplesStreamed := 0
+	for i := range samples {
+		if len(s.buffer) == 0 {
+			break
+		}
+
+		samples[i] = s.buffer[i]
+		numSamplesStreamed++
+	}
+
+	s.buffer = s.buffer[numSamplesStreamed:]
+
+	if len(s.buffer) < s.minBufferSize {
+		s.start()
+	}
+
+	return numSamplesStreamed, true
 }
 
 func (s *Streamer) Err() error {
@@ -63,4 +96,10 @@ func (s *Streamer) start() {
 
 func (s *Streamer) stop() {
 	s.device.Stop()
+}
+
+func sampleBytesToFloats(input []byte, sampleCount int) [][2]float64 {
+	samples := make([][2]float64, sampleCount)
+
+	return samples
 }
